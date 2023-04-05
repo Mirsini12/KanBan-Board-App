e35cbf6e7a69837e40b4ea6946969e7bffecb56b
Mirsini12 <mirsininousi12@gmail.com>
2023-04-05 22:31:00 +0100

    changes in tasks.js

--- server/Routers/tasks.js
+++ server/Routers/tasks.js
@@ -2,32 +2,23 @@ const express = require("express");
 const router = express.Router();
 const Task = require("../Models/tasksModels");
 
-
-//get all tasks
+// get tasks with the specific status in the form http://localhost:8000/tasks?status=status
 router.get("/", async (req, res) => {
-    try {
-      let tasks = await Task.find();
-      res.send(tasks);
-    } catch (error) {
-      res.send({ msg: error })
+  try {
+    const status = req.query.status;
+    let statusTasks;
+    if (status) {
+       statusTasks = await Task.find({ status });
+    }else{
+      statusTasks=await Task.find();
     }
-  
-  })
-  
-  // get tasks with the specific status in the form http://localhost:8000/tasks?status=status
-  router.get("/", async (req, res) => {
-    try {
-      const status = req.query.status;
-    const tasks = await Task.find({status} );   
-    res.send(tasks).status(200);
-      
-    } catch (error) {
-      res.send({ msg: error })
-    }
-    
-  });
+    res.send(statusTasks).status(200);
+  } catch (error) {
+    res.send({ msg: error });
+  }
+});
 
-  // get tasks with the specific status  http://localhost:8000/tasks/status
+// get tasks with the specific status  http://localhost:8000/tasks/status
 //   router.get('/:status', async (req, res) => {
 //     const status = req.params.status
 
@@ -35,37 +26,32 @@ router.get("/", async (req, res) => {
 
 //     res.send(tasks).status(200)
 // });
-  
-  router.post("/create", async (req, res) => {
-    try {
-      let newTask = await Task.create(req.body);
-      res.send({ msg: "A new todo created ", newTask });
-    } catch (error) {
-      res.send({ msg: error })
-    }
-  
-  })
-  
-  router.put("/update/:id", async (req, res) => {
-    try {
-      let updatedTask = await Task.updateOne({ _id: req.params.id }, req.body);
-      res.send({ msg: "User updated ", updatedTask });
-  
-    } catch (error) {
-      res.send({ msg: error })
-    }
-  
-  })
-  
-  router.delete("/delete/:id", async (req, res) => {
-    try {
-      let deletedTask = await Task.deleteOne({ _id: req.params.id });
-      res.send({ msg: "User deleted ", deletedTask });
-    } catch (error) {
-      res.send({ msg: error })
-    }
-  
-  })
-  
-  
-  module.exports = router;
+
+router.post("/create", async (req, res) => {
+  try {
+    let newTask = await Task.create(req.body);
+    res.send({ msg: "A new task created ", newTask });
+  } catch (error) {
+    res.send({ msg: error })
+  }
+})
+
+router.put("/:id", async (req, res) => {
+  try {
+    let updatedTask = await Task.updateOne({ _id: req.params.id }, req.body);
+    res.send({ msg: "User updated ", updatedTask });
+  } catch (error) {
+    res.send({ msg: error })
+  }
+})
+
+router.delete("/:id", async (req, res) => {
+  try {
+    let deletedTask = await Task.deleteOne({ _id: req.params.id });
+    res.send({ msg: "User deleted ", deletedTask });
+  } catch (error) {
+    res.send({ msg: error })
+  }
+})
+
+module.exports = router;
