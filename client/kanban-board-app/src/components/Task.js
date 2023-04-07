8257ff993efad8726c90dfdd9d11f8b8f688c5b2
Mirsini12 <mirsininousi12@gmail.com>
2023-04-08 00:24:48 +0100

    add useDeleteTask function handleDelete function

--- client/kanban-board-app/src/components/Task.js
+++ client/kanban-board-app/src/components/Task.js
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Card, CardHeader, CardBody, CardFooter,Heading,Button } from '@chakra-ui/react';
-const Task = ({task}) => {
-    
-    return (
-        <Card>
-            <CardHeader><Heading size={'sm'}>{task.task}</Heading>  </CardHeader>
-            <CardBody>{task.description}</CardBody>
-            <CardFooter><Button >Delete</Button></CardFooter>
-        </Card>
-    );
+import { Card, CardHeader, CardBody, CardFooter, Heading, IconButton } from '@chakra-ui/react';
+import { DeleteIcon } from '@chakra-ui/icons';
+import { useDeleteTask } from '../data/hooks';
+const Task = ({ task }) => {
+ const { mutate: deleteTask } = useDeleteTask(task.status);
+ const handleDelete = () => {
+  deleteTask(task._id)
+ }
+ return (
+  <Card>
+   <CardHeader><Heading size={'sm'}>{task.task}</Heading>  </CardHeader>
+   <CardBody>{task.description}</CardBody>
+   <CardFooter>
+    <IconButton size={'sm'} onClick={handleDelete} mr={4} icon={<DeleteIcon />}> </IconButton>
+   </CardFooter>
+  </Card>
+ );
 }
 
 export default Task;
--- client/kanban-board-app/src/data/hooks.js
+++ client/kanban-board-app/src/data/hooks.js
@@ -1,19 +1,26 @@
-import { useMutation, useQuery, QueryClient} from "react-query";
+import { useMutation, useQuery, QueryClient } from "react-query";
 import axios from "axios";
 
 export const queryClient = new QueryClient();
 
-export function useFetchTasks(status){
-    return useQuery({
-     queryKey:[status],
-     queryFn:()=> axios.get(`http://localhost:8000/tasks?status=${status}`)
-     .then(res=>res.data)
-    })
- }
+export function useFetchTasks(status) {
+ return useQuery({
+  queryKey: [status],
+  queryFn: () => axios.get(`http://localhost:8000/tasks?status=${status}`)
+   .then(res => res.data)
+ })
+}
 
- export function useCreateTask(){
-    return useMutation({
-       mutationFn:(request)=>axios.post("http://localhost:8000/tasks",request),
-       onSuccess:()=> queryClient.invalidateQueries({queryKey:['new']})
-    })
+export function useCreateTask() {
+ return useMutation({
+  mutationFn: (request) => axios.post("http://localhost:8000/tasks", request),
+  onSuccess: () => queryClient.invalidateQueries({ queryKey: ['new'] })
+ })
+}
+
+export function useDeleteTask(status) {
+ return useMutation({
+  mutationFn: (id) => axios.delete(`http://localhost:8000/tasks/${id}`),
+  onSuccess: () => queryClient.invalidateQueries({ queryKey: [status] })
+ })
 }
